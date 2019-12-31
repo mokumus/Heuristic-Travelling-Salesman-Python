9e7e51d7ab4a244084ccfa0764056571f7f48109
Muhammed Okumuş <rocketpolka@gmail.com>
2019-12-31 22:21:49 +0300

    More ACO methods implemented

--- aco.py
+++ aco.py
@@ -12,7 +12,7 @@ def initialise_pheromones(problem, init_pher) :
 def calculate_choices(problem, last_city, exclude, pheromone_map, c_heur, c_hist):
 	choices = []
 	for city in problem.get_nodes():
-		if city != exclude and last_city != city:
+		if city not in exclude:
 			p = dict()
 			p['city']       = city
 			p['history']    = pheromone_map[(last_city,city)] ** c_hist
@@ -29,20 +29,42 @@ def probabilistic_select(choices) :
 		return choices[random.randint(0,len(choices))]['city']
 
 	v = random.random()
-	for choice in choices: # Choose city with highest probability
-		v -= (choice['probability'] / p_sum)
+	for choice in choices: # Choose good city near a high probability city
+		v -= choice['probability'] / p_sum
 		if v <= 0.0:
 			return choice['city']
 
 	return choices[-1]['city'] # Choose last city if no choice is made to this point
 
+def greedy_select(choices) :
+	return max(choices, key=lambda x:x['probability'])['city']  # Choose city with highest probability
 
+def stepwise_const(problem, pheromone_map, c_heur, c_greed) :
+	perm = [random.randint(1, problem.dimension)] # Start path at random point
+
+	while len(perm) < problem.dimension :
+		choices = calculate_choices(problem, perm[-1], perm, pheromone_map, c_heur, 1.0)
+		is_greedy = random.random() <= c_greed
+		next_city = greedy_select(choices) if is_greedy else probabilistic_select(choices)
+		perm.append(next_city)
+	return perm
+
+def global_update_pheromone(problem, pheromone_map, candidate_path, decay_amount) :
+	for i in range(0,len(candidate_path)-1):
+		edge_forward = (candidate_path[i], candidate_path[i+1])
+		edge_back    = (candidate_path[i+1], candidate_path[i])
+		value = ((1.0 - decay_amount) * pheromone_map[edge_forward]) + (decay_amount * (1.0 / utils.cost(candidate_path,problem)))
+		pheromone_map[edge_forward] = value
+		pheromone_map[edge_back]	= value
 
 if __name__ == '__main__':
 	problem = tsplib95.load_problem('problems/berlin52.tsp')
 	problem.best_known = 7544.3659
-	pheromon_map = initialise_pheromones(problem, 3.0)
-	choices = calculate_choices(problem, 3, 1, pheromon_map, 0.9, 2.0)
-	print(probabilistic_select(choices))
+	pheromone_map = initialise_pheromones(problem, 3.0)
+	stepwise_const(problem, pheromone_map, 2.5, 0.9)
+	perm = utils.random_permutation([*range(1, problem.dimension + 1)])
+	print(perm)
+	global_update_pheromone(problem,pheromone_map,perm, 0.1)
+
 
 
